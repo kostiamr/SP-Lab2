1b7c0166b1bda64db09775e9adfe93ef384b7eba
Kostiantyn Martsyniuk <kmartsinuk@gmail.com>
2024-12-29 02:04:50 +0200

    Add files and finish task

---
+++ src/calculator.cpp
@@ -0,0 +1,11 @@
+#include "calculator.h"
+
+int Add(const int a, const int b)
+{
+    return a + b;
+}
+
+int Sub(const int a, const int b)
+{
+    return Add(a, -b);
+}
---
+++ src/calculator.h
@@ -0,0 +1,12 @@
+//
+// Created by Asus on 12/29/2024.
+//
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+
+int Add(int, int);
+int Sub(int, int);
+
+
+#endif//CALCULATOR_H
